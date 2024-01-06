eb5f9dc59f0438355d533c70314f9424e6048075
deenomax <lonaberdeen@gmail.com>
2024-01-05 18:17:39 -0800

    Initial Commit

---
+++ Weather App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Weather App
+//
+//  Created by Londell Aberdeen on 1/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Londell Aberdeen on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
